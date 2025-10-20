8f38c3b59f7448b7db08ffb974e9678f16a3d671
Kossay-Zemzem <kossayzemzem@gmail.com>
2025-10-21 00:15:13 +0100

    Update comment  about dev environnement

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
-//This file contains variables used in development
+//This file contains variables used in development (default when running `ng serve`)
 //see /src/environments/environment.ts for variables used in production 
 export const environment = {
     production: false,
